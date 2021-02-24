da722418b4f4507356a513e2bcf4a59f7ec61988
Tomas Ramos <tomas.ramosv21@gmail.com>
2021-02-24 21:47:12 +0800

    Update config.el

--- config.el
+++ config.el
@@ -24,7 +24,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
+(setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -52,6 +52,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 (setq projectile-project-search-path '("/Users/tomasramos/Repos/"))
+(setq-default line-spacing 4)
 (after! org
   (setq org-hide-emphasis-markers t)
   (setq org-agenda-span 20))
