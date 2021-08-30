af53c42a692777e13b3c3787a67e5834be7e8b2f
Tomas Ramos <tomas.ramos@apple.com>
2021-08-30 20:58:32 +0800

    Updated config

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(disaster-objdump "objdump -d -Sl --no-show-raw-insn")
+ '(package-selected-packages '(spacemacs-theme))
+ '(safe-local-variable-values
+   '((eval setq flycheck-clang-include-path
+           '("~/Repos/Donut/inc/" disaster-objdump "objdump -d -Sl --no-show-raw-insn"))
+     (eval setq flycheck-clang-include-path
+           '("~/Repos/Donut/inc/")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
